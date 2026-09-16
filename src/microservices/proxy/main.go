b930f04358f7665f16229469f75a44fb45640b81
Артем Бакулев <a.bakulev@ktsstudio.ru>
2026-09-16 21:43:32 +0300

    fix proxy dockerfile binary name, listen address and upstream url parsing

--- src/microservices/proxy/main.go
+++ src/microservices/proxy/main.go
@@ -16,13 +16,12 @@ import (
 
 func getProxyRewriteFunc(backend url.URL) func(*httputil.ProxyRequest) {
 	return func(pr *httputil.ProxyRequest) {
-		pr.In.URL.Scheme = backend.Scheme
-		pr.In.URL.Host = backend.Host
+		pr.Out.URL.Scheme = backend.Scheme
+		pr.Out.URL.Host = backend.Host
 	}
 }
 
 func main() {
-
 	monolith_url, err := url.Parse(os.Getenv("MONOLITH_URL"))
 	if err != nil || monolith_url.Host == "" {
 		log.Fatal("Invalid $MONOLITH_URL")
@@ -42,7 +41,7 @@ func main() {
 		log.Fatal("Invalid $PORT")
 	}
 
-	proxy_address := fmt.Sprintf("127.0.0.1:%d", port)
+	proxy_address := fmt.Sprintf("0.0.0.0:%d", port)
 	log.Printf("Starting reverse proxy listens on %s port", proxy_address)
 
 	err = http.ListenAndServe(proxy_address, proxy)
